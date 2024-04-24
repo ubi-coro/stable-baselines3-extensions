feba2ace61d7a9abfa86cb457b869d1dcb99a071
Lara Bergmann <lbergmann@techfak.uni-bielefeld.de>
2024-04-24 18:35:34 +0200

    save_system_info: check if save_path exists

--- stable_baselines3_extensions/common/utils.py
+++ stable_baselines3_extensions/common/utils.py
@@ -16,5 +16,9 @@ def save_system_info(save_path: str) -> None:
     system_info_dict.update({"GPU Enabled": torch.cuda.is_available()})
     for dist in metadata.distributions():
         system_info_dict.update({dist.name: dist.version})
+        
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+        
     with open(os.path.join(save_path, "system_info.txt"), "w") as f:
         json.dump(system_info_dict, f, indent=2)
