c9b7473f64b6c5f4c1db3386ddc1dc08d47a6fdf
Lara Bergmann <lbergmann@techfak.uni-bielefeld.de>
2024-04-24 18:02:30 +0200

    v0.0.2: ensure reproducibility

--- stable_baselines3_extensions/__init__.py
+++ stable_baselines3_extensions/__init__.py
@@ -1,7 +1,13 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
+import os
+import torch
 from stable_baselines3_extensions.her.her_replay_buffer import HerReplayBufferExt
 
 __all__ = [
     "HerReplayBufferExt",
 ]
+
+if torch.cuda.is_available():
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+torch.use_deterministic_algorithms(True)
